4eafddc32c1c5f54b6392f318b292460aa45296e
ARCassiano <anderson.cassiano@hotmail.com>
2018-11-07 19:57:34 -0200

    Exemplo básico

---
+++ MeuApp/MeuApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MeuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mensagem = "Hello World!";
+            Console.WriteLine(mensagem);
+            Console.ReadKey();
+        }
+    }
+}
